b9d5a5a8360061b1c7a0c4eecbd7ecbb4b77dcec
Jonas Kalsvik <jonas@kalsvik.no>
2025-08-01 19:29:46 +0200

    untracked backgrounds.ts

---
+++ src/routes/download/+page.server.ts
@@ -0,0 +1,71 @@
+import { error, ServerLoad } from "@sveltejs/kit";
+let startedCheckRoutine = false;
+let cachedJson: any;
+
+async function CacheReleases() {
+  console.log("Caching new releases");
+  let releaseRequest = await fetch(
+    "https://api.github.com/repos/Epic-Mickey-Launcher/launcher/releases",
+  );
+  if (releaseRequest.status != 200) {
+    console.log("github api returned wrong result: " + releaseRequest.status);
+    return;
+  }
+  cachedJson = await releaseRequest.json();
+}
+
+function CheckRoutine() {
+  if (!startedCheckRoutine) {
+    startedCheckRoutine = true;
+    console.log("Check Routine");
+
+    setInterval(async () => {
+      await CacheReleases();
+    }, 1800 * 1000);
+  }
+}
+
+export const prerender = true;
+export const load: ServerLoad = async () => {
+  let releaseInfo = {
+    tag_name: "",
+    download_url: "",
+    stability: "",
+  };
+
+  let json: any = cachedJson;
+
+  if (!startedCheckRoutine) {
+    CheckRoutine();
+    await CacheReleases();
+    json = cachedJson;
+  }
+
+  if (json == null) {
+    return error(500, "Couldn't get latest releases from GitHub.");
+  }
+
+  releaseInfo.tag_name = json[0].tag_name;
+  releaseInfo.download_url = json[0].html_url;
+  if (json[0].prerelease) {
+    releaseInfo.stability = "unstable";
+  }
+
+  let userCountRequest = await fetch("https://emlapi.kalsvik.no/user/count");
+  let count = await userCountRequest.text();
+  let memberCount = Number(count);
+  let futureMemberCount = (memberCount + 1).toString();
+  let memberCountSuffix = "";
+  if (futureMemberCount.endsWith("1")) {
+    memberCountSuffix = "st";
+  } else if (futureMemberCount.endsWith("2")) {
+    memberCountSuffix = "nd";
+  } else if (futureMemberCount.endsWith("3")) {
+    memberCountSuffix = "rd";
+  } else {
+    memberCountSuffix = "th";
+  }
+
+  let formattedMemberCount = String(memberCount) + memberCountSuffix;
+  return { releaseInfo, formattedMemberCount };
+};
---
+++ src/routes/library/backgrounds.ts
@@ -0,0 +1,59 @@
+export let downloadPageBackgrounds = [
+  {
+    src: "download/back1.jpg",
+    credit: "Disney Epic Mickey 2 Concept Art",
+  },
+  {
+    src: "download/back2.jpg",
+    credit: "Disney Epic Mickey 2 Concept Art",
+  },
+  {
+    src: "download/back3.png",
+    credit: "Disney Epic Mickey w/ Authentic Oswald Cosplay EML Mod",
+  },
+  {
+    src: "download/back4.png",
+    credit: "Disney Epic Mickey w/ Authentic Oswald Cosplay EML Mod",
+  },
+  {
+    src: "download/back5.png",
+    credit: "Disney Epic Mickey w/ Authentic Oswald Cosplay EML Mod",
+  },
+  {
+    src: "download/back6.png",
+    credit: "Disney Epic Mickey",
+  },
+  {
+    src: "download/back7.png",
+    credit: "Disney Epic Mickey",
+  },
+  {
+    src: "download/back8.png",
+    credit: "Disney Epic Mickey w/ Epic Mickey Awesome Edition EML Mod",
+  },
+  {
+    src: "download/back9.png",
+    credit: "Disney Epic Mickey w/ Epic Mickey Awesome Edition EML Mod",
+  },
+  {
+    src: "download/back10.png",
+    credit: "Disney Epic Mickey 2 w/ FNATI Skins for Mickey and Oswald EML Mod",
+  },
+  {
+    src: "download/back11.png",
+    credit: "Disney Epic Mickey 2",
+  },
+  {
+    src: "download/back12.png",
+    credit: "Disney Epic Mickey 2",
+  },
+  {
+    src: "download/back13.png",
+    credit: "Disney Epic Mickey 2",
+  },
+  {
+    src: "download/back14.png",
+    credit: "Disney Epic Mickey 2 w/ FNATI Skins for Mickey and Oswald EML Mod",
+  },
+  { src: "download/back16.jpg", credit: "Disney Epic Mickey Rebrushed" },
+];
