9c6be7df6d1a1c72b42236e270a52c3b67ff7b5f
Jonas Kalsvik <jonas@kalsvik.no>
2025-08-03 00:10:32 +0200

    added cool transition when background image is loaded

--- src/routes/components/background.svelte
+++ src/routes/components/background.svelte
@@ -21,6 +21,7 @@
     preload.style.display = "none";
     preload.onload = () => {
       background.style.opacity = "1";
+      background.style.transform = "scale(1)";
       preload.remove();
     };
     preload.src = imgPath;
@@ -30,7 +31,11 @@
 </script>
 
 <div class="background-placeholder"></div>
-<div style="opacity:0;" bind:this={background} class="background"></div>
+<div
+  style="opacity:0;transform:scale(1.1);"
+  bind:this={background}
+  class="background"
+></div>
 <span
   style="position: fixed; bottom: 0px; left:0px;background-color: rgba(30, 30, 30, 0.5);padding: 10px;border-radius: 5px;font-size: 12px;"
   >{credit}</span
@@ -40,9 +45,10 @@
   .background {
     opacity: 0;
     transition-duration: 1s;
-    transition-property: opacity;
+    transition-property: opacity, transform;
     background-size: cover;
     background-position: center;
+    transform: scale(1.1);
     width: 100%;
     height: 100%;
     position: fixed;
@@ -57,7 +63,7 @@
     width: 100%;
     height: 100%;
     position: fixed;
-    background-color: black;
+    background-color: rgb(10, 10, 10);
     top: 0px;
     left: 0px;
     z-index: -6;
